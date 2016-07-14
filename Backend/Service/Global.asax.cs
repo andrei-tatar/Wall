856e5f5bbd22fac3e3b9f288b57aebd1db0e0f5b
Andrei Tatar <tatarandrei@gmail.com>
2016-07-14 20:51:59 +0300

    add cors support for service

--- Backend/Service/Global.asax.cs
+++ Backend/Service/Global.asax.cs
@@ -1,25 +1,30 @@
 ﻿using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Newtonsoft.Json.Serialization;
 
 namespace Service
 {
-    public class Application : HttpApplication
-    {
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(config =>
-            {
-                config.MapHttpAttributeRoutes();
+	public class Application : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			GlobalConfiguration.Configure(config =>
+			{
+				//the frontend is on a different endpoint. allow cross domain requests
+				var cors = new EnableCorsAttribute("*", "*", "*");
+				config.EnableCors(cors);
 
-                config.Routes.MapHttpRoute(
-                    name: "DefaultApi",
-                    routeTemplate: "api/{controller}/{id}",
-                    defaults: new { id = RouteParameter.Optional }
-                );
+				config.MapHttpAttributeRoutes();
 
-                config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            });
-        }
-    }
+				config.Routes.MapHttpRoute(
+						 name: "DefaultApi",
+						 routeTemplate: "api/{controller}/{id}",
+						 defaults: new { id = RouteParameter.Optional }
+					);
+
+				config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+			});
+		}
+	}
 }
