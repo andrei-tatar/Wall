29f1adf31c3352f822af3a3b04ba8167cb31860e
andrei-tatar <tatarandrei@gmail.com>
2016-07-18 18:11:01 +0300

    progress on login service

--- Frontend/src/app/services/loginService.ts
+++ Frontend/src/app/services/loginService.ts
@@ -1,14 +1,22 @@
-import {Service} from '../util';
+import {Service, Inject} from '../util';
 import {ILoginService} from '../interfaces/services';
 
 @Service('LoginService')
+@Inject('$http', 'apiUri')
 export class LoginService implements ILoginService {
+
+    constructor(private http: ng.IHttpService,
+                private apiUri: string) {
+    }
 
     public isLoggedIn(): boolean {
         return false;
     }
 
     public login(user: string, password: string): ng.IPromise<any> {
-        return undefined;
+        return this.http.post(this.apiUri + 'api/login', {
+            user: user,
+            password: password
+        });
     }
 }
