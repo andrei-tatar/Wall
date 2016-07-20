e9705270ed6e2723f38abbba3ba8113688ae4072
andrei-tatar <tatarandrei@gmail.com>
2016-07-20 13:07:04 +0300

    fix memory leak

--- Backend/Service/MefDependencyResolver.cs
+++ Backend/Service/MefDependencyResolver.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
 using System.Linq;
 using System.Web.Http.Dependencies;
 
@@ -9,25 +11,36 @@ namespace Service
     public class MefDependencyResolver : IDependencyResolver
     {
         private readonly CompositionContainer _container;
+        private readonly ComposablePartCatalog _catalog;
+        private static readonly string CreationPolicyKey = typeof(CreationPolicy).FullName;
 
         public MefDependencyResolver()
         {
-            _container = new CompositionContainer(new ApplicationCatalog());
+            _catalog = new ApplicationCatalog();
+            _container = new CompositionContainer(_catalog);
         }
 
-        private MefDependencyResolver(ExportProvider parent)
+        private MefDependencyResolver(ExportProvider parent, ComposablePartCatalog parentCatalog)
         {
-            _container = new CompositionContainer(parent);
+            _container = new CompositionContainer(new FilteredCatalog(parentCatalog, Filter), parent);
+        }
+
+        private static bool Filter(ComposablePartDefinition definition)
+        {
+            object creationPolicyObj;
+            return definition.Metadata.TryGetValue(CreationPolicyKey, out creationPolicyObj) &&
+                   (CreationPolicy)creationPolicyObj == CreationPolicy.NonShared;
         }
 
         public void Dispose()
         {
             _container.Dispose();
+            _catalog?.Dispose();
         }
 
         public IDependencyScope BeginScope()
         {
-            return new MefDependencyResolver(_container);
+            return new MefDependencyResolver(_container, _catalog);
         }
 
         public object GetService(Type serviceType)
