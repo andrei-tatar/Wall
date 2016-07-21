63f68f16f13f5268fc34dcae07b44059f02cd0a9
andrei-tatar <tatarandrei@gmail.com>
2016-07-21 15:18:08 +0300

    add app.css with style for the main view

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -6,6 +6,7 @@ import 'angular-ui-router';
 var app = angular.module('wall-app', ['ngMaterial', 'ui.router', 'ngMessages']);
 export { app };
 
+import './app.css!';
 import './config';
 import './routes';
 import './values';
